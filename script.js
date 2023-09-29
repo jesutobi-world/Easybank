0a5f865f4c850b240691b95bd6f53fc6f88e2edd
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2023-09-29 12:02:46 +0100

    mobie-menu

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-// Nav Toggle Functionality
-let menuButton = document.querySelector('.menu')
-let closeButton = document.querySelector('.close')
-let mobileMenu = document.querySelector('.mobile-menu')
 
-function menuToggle(){
+function menuToggle() {
+    let menuButton = document.querySelector('.menu')
+    let closeButton = document.querySelector('.close')
+    let mobileMenu = document.querySelector('.mobile-menu')
+
     menuButton.classList.toggle('hidden');
     closeButton.classList.toggle('hidden');
     mobileMenu.classList.toggle('hidden');
-    // mobileMenu.classList.toggle('transform');
 }
+
+// Event listener to hide the menu when the user starts scrolling
+var prevScrollPos = window.scrollY;
+window.addEventListener('scroll', function () {
+    let currentScrollPos = window.scrollY;
+    let mobileMenu = document.querySelector('.mobile-menu');
+
+    if (prevScrollPos < currentScrollPos && !mobileMenu.classList.contains('hidden')) {
+        menuToggle();
+    }
+    prevScrollPos = currentScrollPos;
+});
