056a2d2f5304c210d608bae3e9e7d5b2c0331b0b
Bada Jesutobi <jbada467@stu.ui.edu.ng>
2023-09-29 01:42:59 +0100

    mobile view

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'hero': "url(/images/bg-intro-mobile.svg)"
+      },
+      textColor:{
+        'darkBlue' : 'hsl(233, 26%, 24%)',
+        'grayishBlue' :'hsl(233, 8%, 62%)',
+      },
+      colors:{
+        'limeGreen' : 'hsl(136, 65%, 51%)',
+        'brightCyan' : 'hsl(192, 70%, 51%)',
+        'lightGrayishBlue': 'hsl(220, 16%, 96%)',
+        'veryLightGrayishBlue':'hsl(0, 0%, 98%)',
+        'grayishBlue' :'hsl(233, 8%, 62%)',
+        'darkBlue' : 'hsl(233, 26%, 24%)',
+      },
+      fontFamily:{
+        'publicSans' : ["'Public Sans'", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
